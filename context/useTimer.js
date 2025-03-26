c656c50072257fa06d32a61b7193e7ea659609f6
shakir9160 <shakir9160@gmail.com>
2025-03-26 13:16:27 +0530

    updated timer.js for getting crashed

--- context/useTimer.js
+++ context/useTimer.js
@@ -1,14 +1,26 @@
 import { useState, useEffect } from "react";
 
-const useTimer = () => {
-  const [seconds, setSeconds] = useState(() => {
-    return parseInt(localStorage.getItem(`timer-${location}`)) || 0;
-  });
+const useTimer = (location) => {
+  const [seconds, setSeconds] = useState(0);
   const [running, setRunning] = useState(false);
+  const [isClient, setIsClient] = useState(false); // Prevent SSR issues
 
   useEffect(() => {
-    localStorage.setItem(`timer-${location}`, seconds);
-  }, [seconds]);
+    setIsClient(true); // Set to true once mounted on client
+  }, []);
+
+  useEffect(() => {
+    if (isClient) {
+      const savedTime = parseInt(localStorage.getItem(`timer-${location}`)) || 0;
+      setSeconds(savedTime);
+    }
+  }, [isClient, location]);
+
+  useEffect(() => {
+    if (isClient) {
+      localStorage.setItem(`timer-${location}`, seconds);
+    }
+  }, [seconds, isClient, location]);
 
   useEffect(() => {
     let interval;
